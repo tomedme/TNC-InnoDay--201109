8e77e8b5377e13a6fc5a8eedb64a4138eaa4ea76
Fabien Basmaison <fabien@thenetcircle.com>
2011-09-14 11:21:53 +0800

    first commit: basic HTML5 template

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Stats 4 Square</title>
+  <link rel="stylesheet" href="/css/main.css" />
+</head>
+
+<body>
+  <section>
+    Here goes the Map
+  </section>
+  <section>
+    Here goes the Object
+  </section>
+</body>
+</html>
