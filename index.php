14b09454ccccc4d803d06c46a7abeb352693b473
Fabien Basmaison <fabien@thenetcircle.com>
2011-09-14 16:55:31 +0800

    removed root for links + start JS scripting

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
   <link rel="stylesheet" type="text/css" media="screen" href="/media/main.css" />
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
   <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-  <script type="text/javascript" src="/media/gmap.js"></script>
-  <script type="text/javascript" src="/media/4sq.js"></script>
-  <script type="text/javascript" src="/media/app.js"></script>
+  <script type="text/javascript" src="media/gmap.js"></script>
+  <script type="text/javascript" src="media/4sq.js"></script>
+  <script type="text/javascript" src="media/app.js"></script>
 </head>
 
 <body>
@@ -20,5 +20,31 @@
       Your browser doesn't seem to support SVG (or else, you wouldn't see that message).
     </object>
   </section>
+
+  <script>
+    (function(){
+
+      var target = $('#category-bars');
+      alert(target);
+      // create the <animation> element
+      var animation = document.createElementNS(
+      'http://www.w3.org/2000/svg', 'animate');
+      // set its attributes
+      animation.setAttributeNS(null, 'attributeName', 'opacity');
+      animation.setAttributeNS(null, 'begin', 'indefinite');
+      animation.setAttributeNS(null, 'to', 0);
+      animation.setAttributeNS(null, 'dur', 0.25);
+      animation.setAttributeNS(null, 'fill', 'freeze');
+      // link the animation to the target
+      target.append(animation);
+
+      // start the animation
+      animation.beginElement();
+
+
+
+
+    })();
+  </script>
 </body>
 </html>
