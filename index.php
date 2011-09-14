8ef36a14c7f8ffb176cdd7fe39bbfae83a08ce04
Tom Edme <tedme@thenetcircle.com>
2011-09-14 12:38:21 +0800

    app basics

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
   <meta charset="utf-8" />
   <title>Stats 4 Square</title>
   <link rel="stylesheet" href="/media/main.css" />
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
+  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+  <script src="/media/gmap.js"></script>
+  <script src="/media/4sq.js"></script>
+  <script src="/media/app.js"></script>
 </head>
 
 <body>
