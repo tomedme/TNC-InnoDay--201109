dc74d2e3a75af0b6fb8a70eed9f950795e2ad43c
Tom Edme <tedme@thenetcircle.com>
2011-09-14 11:51:24 +0800

    media directory for css and js

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <title>Stats 4 Square</title>
-  <link rel="stylesheet" href="/css/main.css" />
+  <link rel="stylesheet" href="/media/main.css" />
 </head>
 
 <body>
